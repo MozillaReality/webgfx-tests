20900d558ffe019912701b377b2f314e65f4ca6e
Fernando Serrano <fernandojsg@gmail.com>
2018-09-30 17:02:52 +0200

    Added mouse click div

--- src/client/input-helpers.js
+++ src/client/input-helpers.js
@@ -1,19 +1,48 @@
-import KeystrokeVisualizer from 'keystroke-visualizer';
+//import KeystrokeVisualizer from 'keystroke-visualizer';
 
 export default class InputHelpers {
   initKeys() {
-    KeystrokeVisualizer.enable({unmodifiedKey: false});
+  //  KeystrokeVisualizer.enable({unmodifiedKey: false});
   }
 
   initMouse() {
     this.mouseDiv = document.createElement('div');
-    this.mouseDiv.style.cssText="position:absolute;width:30px; height:30px; left:0px; top:0px; background-image:url('../cursor.svg');";
+    this.mouseDiv.id='mousediv';
+    this.mouseClick = document.createElement('div');
+    this.mouseClick.id='mouseclick';
+    this.mouseClick.style.cssText = `
+      border-radius: 50%;
+      width: 30px;
+      height: 30px;
+      background: yellow;
+      position: absolute;
+      left: 0px;
+      top: 0px;
+      border: 3px solid black;
+      opacity: 0.5;
+      visibility: hidden;
+    `;
+
+    this.mouseDiv.style.cssText = "position:absolute;width:30px; height:30px; left:0px; top:0px; background-image:url('../cursor.svg');z-index:9999";
     
     this.canvas.parentNode.appendChild(this.mouseDiv);
+    this.canvas.parentNode.appendChild(this.mouseClick);
+
     this.canvas.addEventListener('mousemove', (evt) => {
       this.mouseDiv.style.left = evt.x + "px";
       this.mouseDiv.style.top = evt.y + "px";
+
+      this.mouseClick.style.left = `${evt.x - 3}px`;
+      this.mouseClick.style.top = evt.y + "px";
     });
+
+    this.canvas.addEventListener('mousedown', evt => {
+      this.mouseClick.style.visibility = 'visible';
+    });
+    this.canvas.addEventListener('mouseup', evt => {
+      this.mouseClick.style.visibility = 'hidden';
+    });
+
   }
 
   constructor (canvas, options) {
